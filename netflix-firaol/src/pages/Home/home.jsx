793692c93a5ff9239e6aae6c2837889367ede00d
Firaol Negewo <firaolnegewo8@gmail.com>
2026-09-24 18:46:53 +0300

    Banner

--- netflix-firaol/src/pages/Home/home.jsx
+++ netflix-firaol/src/pages/Home/home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Header from "../../components/Header/Header.jsx";
 import Footer from "../../components/Footer/Footer";
+import Banner from "../../components/Banner/Banner.jsx";
 function Home() {
   return (
     <div>
       <Header />
+       <Banner/>
       <Footer />
+     
     </div>
   );
 }
--- netflix-firaol/src/utils/request.jsx
+++ netflix-firaol/src/utils/request.jsx
@@ -1,4 +1,8 @@
- const API_KEY = process.env.REACT_APP_API_KEY; ;
+//  const API_KEY =process.env.REACT_APP_API_KEY;
+//  const API_KEY = import.meta.env.REACT_APP_API_KEY;
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+ 
 // "42f45349c80c33050cb6eaf024826465";
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
